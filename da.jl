9c7279ea4755c16d6a2b3e92b342d1bcce6cc4fa
Eviatar Bach <eviatarbach@protonmail.com>
2019-05-22 15:10:03 -0700

    Adding more options

--- da.jl
+++ da.jl
@@ -4,9 +4,7 @@ using LinearAlgebra
 
 #addprocs()
 
-m = 20  # ensemble size
-
-@everywhere function lorenz(t, u)
+@everywhere function lorenz(t::Float64, u::Array{Float64, 1})
     σ = 10
     β = 8/3
     ρ = 28
@@ -17,7 +15,8 @@ m = 20  # ensemble size
     return du
 end
 
-@everywhere function rk4(f::Function, y0, t0::Float64, t1::Float64, h::Float64)
+@everywhere function rk4(f::Function, y0::Array{Float64, 1}, t0::Float64,
+                         t1::Float64, h::Float64)
     y = y0
     n = (t1 - t0)÷h
     t = t0
@@ -32,30 +31,17 @@ end
     return y
 end
 
-abstract type Model end
-
-struct ETKF
-    ens::Array{Float64, 2}
-    m::Int64
-    H::Array{Float64, 2}
-    R::Array{Float64, 2}
-    R_inv::Array{Float64, 2}
-    model::Model
-    function ETKF(ens::Array{Float64, 2}, m::Int64, H::Array{Float64, 2},
-                  R::Array{Float64, 2}, model::Model)
-        R_inv = inv(R)
+function run_da(E::Array{Float64, 2}, model::Function,
+                R::Symmetric{Float64, Array{Float64, 2}}, m::Int64;
+                H=I, Δt::Float64=0.1, window::Float64=1.0, cycles::Int64=100)
+    if H != I
+        p, n = size(H)
+    else
+        p, n = size(R)
     end
-end
 
-x0 = [1.0; 0.0; 0.0]
-E = hcat([rk4(lorenz, x0, 0.0, last, 0.1) for last=range(10.0, stop=100.0, length=m)]...)
-
-function run_da(E)
-    Δt = 0.1
-    H = Diagonal(ones(3))
-    R = Diagonal(ones(3))
     R_inv = inv(R)
-    obs_err = MvNormal(zeros(3), diag(R))
+    obs_err = MvNormal(zeros(p), R)
 
     x_true = E[:, end]
     x_free = x_true + rand(obs_err)
@@ -64,9 +50,9 @@ function run_da(E)
 
     errs_free = []
 
-    B = zeros(3, 3)
+    B = zeros(p, p)
 
-    for t=1:100
+    for cycle=1:cycles
         y = x_true + rand(obs_err)
         x_m = mean(E, dims=2)
 
@@ -81,10 +67,15 @@ function run_da(E)
         append!(errs, err)
         append!(errs_free, sqrt(mean((x_free .- x_true).^2)))
 
-        E = hcat(pmap((col)->rk4(lorenz, col, 0.0, Δt, Δt), E[:, i] for i=1:m)...)
+        E = hcat(pmap((col)->rk4(lorenz, col, 0.0, window, Δt), E[:, i] for i=1:m)...)
 
-        x_true = rk4(lorenz, x_true, 0.0, Δt, Δt)
-        x_free = rk4(lorenz, x_free, 0.0, Δt, Δt)
+        x_true = rk4(lorenz, x_true, 0.0, window, Δt)
+        x_free = rk4(lorenz, x_free, 0.0, window, Δt)
     end
     return errs, errs_free
 end
+
+x0 = [1.0; 0.0; 0.0]
+E = hcat([rk4(lorenz, x0, 0.0, last, 0.1) for last=range(10.0, stop=100.0, length=20)]...)
+
+run_da(E, lorenz, Symmetric(diagm(0 => ones(3))), 20)
