79ea8badfeb6ae38bc469ee136389459d0031331
Eviatar Bach <eviatarbach@protonmail.com>
2020-04-15 15:50:20 -0700

    Adding systems

--- models.jl
+++ models.jl
@@ -46,109 +46,6 @@ function model_error_ssa(; x0, model_true, model_err, integrator, outfreq, Δt, 
    return errs#sqrt.(mean(errs, dims=1))
 end
 
-err_pct = 0.1
-
-function lorenz(t, u, p)
-   du = zeros(3)
-   du[1] = p["σ"]*(u[2]-u[1])
-   du[2] = u[1]*(p["ρ"]-u[3]) - u[2]
-   du[3] = u[1]*u[2] - p["β"]*u[3]
-   return du
-end
-
-lorenz_true = (t, u)->lorenz(t, u, Dict("σ" => 10, "β" => 8/3, "ρ" => 28))
-lorenz_err = (t, u)->lorenz(t, u, Dict("σ" => 10.1, "β" => 8/3, "ρ" => 28))
-
-function peña(t, u, p)
-   x, y, z, X, Y, Z = u
-
-   du = zeros(6)
-   du[1] = p["σ"]*(y - x) - p["c"]*(p["S"]*X + p["k1"])
-   du[2] = p["r"]*x - y - x*z + p["c"]*(p["S"]*Y + p["k1"])
-   du[3] = x*y - p["b"]*z + p["c_z"]*Z
-
-   du[4] = p["τ"]*p["σ"]*(Y - X) - p["c"]*(x + p["k1"])
-   du[5] = p["τ"]*p["r"]*X - p["τ"]*Y - p["τ"]*p["S"]*X*Z + p["c"]*(y + p["k1"])
-   du[6] = p["τ"]*p["S"]*X*Y - p["τ"]*p["b"]*Z - p["c_z"]*z
-
-   return du
-end
-
-peña_true = (t, u)->peña(t, u, Dict("σ" => 10, "b" => 8/3, "r" => 28,
-                                    "c" => 0.15, "c_z" => 0, "k1" => 10,
-                                    "S" => 1, "τ" => 0.1))
-
-peña_err = (t, u)->peña(t, u, Dict("σ" => 10 + 0.1, "b" => 8/3, "r" => 28 + 0.1,
-                                    "c" => 0.15 - 0.02, "c_z" => 0, "k1" => 10,
-                                    "S" => 1, "τ" => 0.1))
-
-function ferrari(t, u, p)
-   X, Y, Z, P, Q, ψ_r, ψ_i = u
-
-   F = p["F_0"] + p["F_1"]*cos(p["ω_1"]*t)# + p["F_2"]*cos(p["ω_2"]*(t - p["ϕ"]))
-   du = zeros(7)
-   du[1] = -(Y^2 + Z^2) - p["a"]*X + p["a"]*F + p["r"]*p["f"]*(P - X - p["γ"])
-   du[2] = X*Y - p["b"]*X*Z - Y + p["G"] + p["r"]*p["f"]*(Q - Y)
-   du[3] = X*Z + p["b"]*X*Y - Z
-   du[4] = -(ψ_r^2 + ψ_i^2)*P + p["f"]/p["c"]*(X - P + p["γ"])
-   du[5] = p["f"]/p["c"]*(Y - Q)
-   du[6] = -p["σ"]*ψ_r - p["Ω"]*ψ_i + p["α_r"]*X + p["β_r"]*Y
-   du[7] = p["Ω"]*ψ_r - p["σ"]*ψ_i + p["α_i"]*X + p["β_i"]*Y
-
-   return du
-end
-
-ξ = 8.4e-4
-ω_1 = 2*pi/73
-ω_2 = ω_1*(40/365)
-ferrari_true = (t, u)->ferrari(t, u, Dict("a" => 0.025, "b" => 4, "F_0" => 58.5,
-                                          "F_1" => 19.5, "F_2" => 0, "G" => 1,
-                                          "f" => 1, "r" => 0.23, "c" => 200,
-                                          "γ" => 0.55, "Ω" => 0.0162,
-                                          "σ" => 0.00081, "ξ" => ξ, "α_r" => ξ,
-                                          "α_i" => ξ/2, "β_r" => ξ/2,
-                                          "β_i" => ξ/10, "ω_1" => ω_1,
-                                          "ω_2" => ω_2, "ϕ" => 0.4*ω_2))
-
-ferrari_err = (t, u)->ferrari(t, u,
-                              Dict("a" => 0.025, "b" => 4, "F_0" => 58.5 + 0.5,
-                                   "F_1" => 19.5, "F_2" => 0, "G" => 1,
-                                   "f" => 1, "r" => 0.23, "c" => 200,
-                                   "γ" => 0.55 - 0.05, "Ω" => 0.0162 + 0.005,
-                                   "σ" => 0.00081, "ξ" => ξ, "α_r" => ξ,
-                                   "α_i" => ξ/2, "β_r" => ξ/2,
-                                   "β_i" => ξ/10, "ω_1" => ω_1 - 0.1,
-                                   "ω_2" => ω_2 + 0.1, "ϕ" => 0.2*ω_2))
-
-function rossler(t, u, p)
-   n = 2
-   du = zeros(3*n)
-   for j=1:n
-      x, y, z = u[(j - 1)*3 + 1:(j - 1)*3 + 3]
-      ω = p["ω_0"] + 0.7*(j - 1)
-      du[(j - 1)*3 + 1] = -ω*y - z
-      if j == 1
-         ym1 = y
-      else
-         ym1 = u[(j - 2)*3 + 2]
-      end
-      if j == n
-         yp1 = y
-      else
-         yp1 = u[j*3 + 2]
-      end
-      du[(j - 1)*3 + 2] = ω*x + p["α"]*y + p["c"]*(yp1 - 2*y + ym1)
-      du[(j - 1)*3 + 3] = 0.4 + z*(x - 8.5)
-   end
-   return du
-end
-
-rossler_true = (t, u)->rossler(t, u, Dict("α" => 0.15, "c" => 0.003,
-                                          "ω_0" => 10))
-
-rossler_err = (t, u)->rossler(t, u, Dict("α" => 0.16, "c" => 0.004,
-                                         "ω_0" => 1 - 0.05))
-
 function colpitts(t, u, p)
    M = 2
 
@@ -265,5 +162,36 @@ osc_true = (t, u)->osc(t, u, Dict("Ω" => 0.3, "σ" => 10, "β" => 8/3,
 
 osc_err = (t, u)->osc(t, u, Dict("Ω" => 0.32, "σ" => 10, "β" => 8/3,
                                  "ρ" => 28, "c" => 5.1))
+
+function ly!(du, t, u, p)
+   θ, y = u
+   #du = zeros(2)
+
+   Λ = p["k_1"]*sin(p["ω_1"]*t) + p["k_2"]*sin(p["ω_2"]*t)
+   du[1] = 2*pi/p["P"] + p["σ"]*y
+   du[2] = -p["λ"]*y + p["γ"]*sin(θ)*Λ
+
+   return du
+end
+
+ly_true = (du, t, u)->ly!(du, t, u, Dict("ω_1" => 2*pi/41, "ω_2" => 2*pi/23,
+                                         "k_1" => 0.8, "k_2" => 0.8, "P" => 100,
+                                         "σ" => 3, "λ" => 0.2, "γ" => 0.1))
+
+function osc84!(du, t, u, p)
+  x, y, z = u
+  #du = zeros(3)
+
+  du[1] = -p["a"]*x - y^2 - z^2 + p["a"]*p["F"]
+  du[2] = -y + x*y - p["b"]*x*z + p["G"]
+  du[3] = -z + p["b"]*x*y + x*z + p["ϵ"]*cos(p["ω"]*t)
+
+  return du
+end
+
+osc84_true = (du, t, u)->osc84!(du, t, u, Dict("a" => 1/4, "b" => 4, "ϵ" => 3,
+                                               "ω" => 2*pi/73, "F" => 11,
+                                               "G" => 0.5))
+
 
 end
