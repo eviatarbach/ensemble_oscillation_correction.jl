31adeb6b241e67825027493b9bbdb42b622b168a
Eviatar Bach <eviatarbach@protonmail.com>
2020-08-25 01:50:50 -0400

    Fixing indexing issue

--- analog.jl
+++ analog.jl
@@ -8,7 +8,7 @@ using NearestNeighbors
 
 function find_point(r, tree, p, k, f)
     ind, dist = knn(tree, p[:], k)
-    mask = (ind .+ f) .<= size(tree.data)[1]
+    mask = (ind .+ f) .<= size(r)[1]
     dist = 1 ./ dist[mask]
     ind = ind[mask]
     return sum(dist .* r[ind .+ f, :], dims=1)/sum(dist)
@@ -16,15 +16,15 @@ end
 
 function error_cov(y, r, M, window, k, k_r, osc_vars; validation_pct=0.1)
     validation = round(Int, validation_pct*size(y)[1])
-    tree = KDTree(copy((y[validation:end, :])'))
-    tree_r = KDTree(copy((r[validation:end, :])'))
+    tree = KDTree(copy((y[validation+1:end, :])'))
+    tree_r = KDTree(copy((r[validation+1:end, :])'))
     errs = Array{Float64}(undef, length(osc_vars), length(M:validation-window))
 
     for (i, i_p) in enumerate(M:validation-window)
         p = y[i_p, :]
-        p_r = find_point(r, tree, p, k, validation + 1)
+        p_r = find_point(r, tree, p, k, validation)
 
-        forecast = find_point(r, tree_r, p_r[:], k_r, validation + window + 1)
+        forecast = find_point(r, tree_r, p_r[:], k_r, validation + window)
         err = r[i_p + window, :] - forecast'
 
         errs[:, i] = err
